35b2e39f1f849ee164ed6216846f8c4ad8715f18
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2026-05-29 20:43:39 +0530

    Create dna_analysis.py

---
+++ code/dna_analysis.py
@@ -0,0 +1,14 @@
+# DNA Sequence Analysis System
+
+dna = "ATGCGTAGCTA"
+
+print("🧬 DNA Sequence:", dna)
+
+# DNA length
+print("📏 Length:", len(dna))
+
+# Count bases
+print("A Count:", dna.count("A"))
+print("T Count:", dna.count("T"))
+print("G Count:", dna.count("G"))
+print("C Count:", dna.count("C"))
